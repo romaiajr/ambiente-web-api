b2e2ee03dabe69203b4b5b976de8050ade6b9482
Roberto Maia <romaiajr5@gmail.com>
2021-02-03 09:56:14 -0300

    ObjetivoProblema CRUD

--- app/Controllers/Http/ObjetivoProblemaController.js
+++ app/Controllers/Http/ObjetivoProblemaController.js
@@ -1,92 +1,112 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Objetivo = use('App/Models/ObjetivoProblema');
+const Database = use('Database');
+const {validateAll} = use('Validator')
 
-/**
- * Resourceful controller for interacting with objetivoproblemas
- */
 class ObjetivoProblemaController {
-  /**
-   * Show a list of all objetivoproblemas.
-   * GET objetivoproblemas
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new objetivoproblema.
-   * GET objetivoproblemas/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
 
   /**
    * Create/save a new objetivoproblema.
    * POST objetivoproblemas
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
+  * ANCHOR STORE
    */
   async store ({ request, response }) {
+    const trx = await Database.beginTransaction();
+    try {
+      const validation = await validateAll(request.all(),{
+        problema_id: 'required|integer',
+        title: 'required|string',
+        description: 'required|string'
+      })
+
+      if(validation.fails()){
+        return response.status(401).send({message: validation.messages()})
+      }
+
+      const dataToCreate = await request.all();
+      const objetivo = await Objetivo.create(dataToCreate,trx);
+      await trx.commit();
+      return response.status(200).send(objetivo);
+      // return response.status(200).send("O requisito foi criado com sucesso!")
+    } catch (error) {
+      await trx.rollback();
+      return response.status(400).send({error: `Erro: ${error.message}`});
+    }
   }
+
 
   /**
    * Display a single objetivoproblema.
    * GET objetivoproblemas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
+    * ANCHOR SHOW
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing objetivoproblema.
-   * GET objetivoproblemas/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show ({ params, request, response, }) {
+    try {
+      const objetivo = await Objetivo.findBy('id', params.id)
+      if(!objetivo){
+        return response.status(404).send({message: 'Nenhum registro localizado'})
+      }
+      return response.status(200).send(objetivo);
+    } catch (error) {
+      return response.status(400).send(`Erro: ${error.message}`)
+    }
   }
 
   /**
    * Update objetivoproblema details.
    * PUT or PATCH objetivoproblemas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
+   * ANCHOR UPDATE
    */
   async update ({ params, request, response }) {
+    const trx = await Database.beginTransaction();
+    try {
+      const validation = await validateAll(request.all(),{
+        title: 'string',
+        description: 'string'
+      })
+
+      if(validation.fails()){
+        return response.status(401).send({message: validation.messages()})
+      }
+      
+      const dataToUpdate = request.all();
+      const objetivo = await Objetivo.findBy('id', params.id)
+
+      if(!objetivo){
+        return response.status(404).send({message: 'Nenhum registro localizado'})
+      }
+
+      objetivo.merge({...dataToUpdate});
+      objetivo.save(trx);
+      await trx.commit();
+      return response.status(200).send(objetivo)
+      // return response.status(200).send("O objetivo foi modificado com sucesso!")
+    } catch (error) {
+      await trx.rollback();
+      return response.status(400).send({erro: `Erro: ${error.message}`})
+    }
   }
 
   /**
    * Delete a objetivoproblema with id.
    * DELETE objetivoproblemas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
+   * ANCHOR DESTROY
    */
   async destroy ({ params, request, response }) {
+    const trx = await Database.beginTransaction();
+    try {
+      const objetivo = await Objetivo.findBy('id', params.id)
+      if(!objetivo){
+        return response.status(404).send({message: 'Nenhum registro localizado'})
+      }
+      await objetivo.delete(trx)
+      await trx.commit();
+      return response.status(200).send("O objetivo do problema foi removido do sistema!");
+    } catch (error) {
+      await trx.rollback();
+      return response.status(400).send({erro: `Erro: ${error.message}`})
+    }
   }
 }
 
