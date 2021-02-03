93c74ab8af1b852ebbcf6df743b1155811ddd792
Roberto Maia <romaiajr5@gmail.com>
2021-02-03 10:31:00 -0300

    ProdutoProblema CRUD

--- app/Controllers/Http/ProdutoProblemaController.js
+++ app/Controllers/Http/ProdutoProblemaController.js
@@ -1,92 +1,113 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Produto = use('App/Models/ProdutoProblema');
+const Database = use('Database');
+const {validateAll} = use('Validator')
 
-/**
- * Resourceful controller for interacting with produtoproblemas
- */
 class ProdutoProblemaController {
-  /**
-   * Show a list of all produtoproblemas.
-   * GET produtoproblemas
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new produtoproblema.
-   * GET produtoproblemas/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
 
   /**
    * Create/save a new produtoproblema.
    * POST produtoproblemas
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
+    * ANCHOR STORE
    */
   async store ({ request, response }) {
+    const trx = await Database.beginTransaction();
+    try {
+      const validation = await validateAll(request.all(),{
+        problema_id: 'required|integer',
+        item_name: 'required|string',
+        amount: 'required|integer'
+      })
+
+      if(validation.fails()){
+        return response.status(401).send({message: validation.messages()})
+      }
+
+      const dataToCreate = await request.all();
+      const produto = await Produto.create(dataToCreate,trx);
+      await trx.commit();
+      return response.status(200).send(produto);
+      // return response.status(200).send("O produto foi criado com sucesso!")
+      
+    } catch (error) {
+      await trx.rollback();
+      return response.status(400).send({error: `Erro: ${error.message}`});
+    }
   }
 
   /**
    * Display a single produtoproblema.
    * GET produtoproblemas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
+    * ANCHOR SHOW
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing produtoproblema.
-   * GET produtoproblemas/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show ({ params, request, response, }) {
+    try {
+      const produto = await Produto.findBy('id', params.id)
+      if(!produto){
+        return response.status(404).send({message: 'Nenhum registro localizado'})
+      }
+      return response.status(200).send(produto);
+    } catch (error) {
+      return response.status(400).send(`Erro: ${error.message}`)
+    }
   }
 
   /**
    * Update produtoproblema details.
    * PUT or PATCH produtoproblemas/:id
    *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
+    * ANCHOR UPDATE
    */
   async update ({ params, request, response }) {
+    const trx = await Database.beginTransaction();
+    try {
+      const validation = await validateAll(request.all(),{
+        item_name: 'string',
+        amount: 'integer'
+      })
+
+      if(validation.fails()){
+        return response.status(401).send({message: validation.messages()})
+      }
+      
+      const dataToUpdate = request.all();
+      const produto = await Produto.findBy('id', params.id)
+
+      if(!produto){
+        return response.status(404).send({message: 'Nenhum registro localizado'})
+      }
+
+      produto.merge({...dataToUpdate});
+      produto.save(trx);
+      await trx.commit();
+      return response.status(200).send(produto)
+      // return response.status(200).send("O produto foi modificado com sucesso!")
+    } catch (error) {
+      await trx.rollback();
+      return response.status(400).send({erro: `Erro: ${error.message}`})
+    }
   }
 
   /**
    * Delete a produtoproblema with id.
    * DELETE produtoproblemas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
+  * ANCHOR DESTROY
    */
   async destroy ({ params, request, response }) {
+    const trx = await Database.beginTransaction();
+    try {
+      const produto = await Produto.findBy('id', params.id)
+      if(!produto){
+        return response.status(404).send({message: 'Nenhum registro localizado'})
+      }
+      await produto.delete(trx)
+      await trx.commit();
+      return response.status(200).send("O produto do problema foi removido do sistema!");
+    } catch (error) {
+      await trx.rollback();
+      return response.status(400).send({erro: `Erro: ${error.message}`})
+    }
   }
 }
 
