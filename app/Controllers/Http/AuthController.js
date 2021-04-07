1f6e5ccdb309121e14d4b0f6dbff806c63f49144
romaiajr <romaiajr5@gmail.com>
2021-04-07 03:20:46 -0300

    Auth changed from email to username

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,20 +1,20 @@
-'use strict'
+"use strict";
 
 class AuthController {
-    // ANCHOR LOGIN
-    async login ({auth, request, response}){
-        try {
-          const {username, password, email} = request.all();
-          const token = await auth.attempt(email,password)
-          // if(username == null)
-          //    validaToken = await auth.attempt(email,password)
-          // else if(email == null)
-          //    validaToken = await auth.attempt(username,password)
-          return token
-        } catch (error) {
-          return response.status(500).send({error: `Erro: ${error.message}`})
-        }
-      }
+  // ANCHOR LOGIN
+  async login({ auth, request, response }) {
+    try {
+      const { username, password, email } = request.all();
+      const token = await auth.attempt(username, password);
+      // if(username == null)
+      //    validaToken = await auth.attempt(email,password)
+      // else if(email == null)
+      //    validaToken = await auth.attempt(username,password)
+      return token;
+    } catch (error) {
+      return response.status(500).send({ error: `Erro: ${error.message}` });
+    }
+  }
 }
 
-module.exports = AuthController
+module.exports = AuthController;
