de64386d3c3280f6eb65c6456866b769c2ce3ab5
romaiajr <romaiajr5@gmail.com>
2021-05-02 23:59:06 -0300

    Disciplina added to Log System

--- app/Controllers/Http/DisciplinaController.js
+++ app/Controllers/Http/DisciplinaController.js
@@ -3,6 +3,7 @@
 const Disciplina = use("App/Models/Disciplina");
 const Database = use("Database");
 const { validateAll, rule } = use("Validator");
+const Log = use("App/Models/SystemLog");
 
 class DisciplinaController {
   /**
@@ -30,36 +31,45 @@ class DisciplinaController {
    * POST disciplinas
    * ANCHOR STORE
    */
-  async store({ request, response }) {
+  async store({ request, response, auth }) {
     const trx = await Database.beginTransaction();
-    try {
-      const validation = await validateAll(request.all(), {
-        code: "string|required|unique:disciplinas,code",
-        name: "string|required",
-        workload: "required|integer",
-        departamento_id: "required|integer",
-      });
-      const rules = await validateAll(request.only(["code", "workload"]), {
-        code: [rule("regex", /\b[A-Z]{3}[0-9]{3}\b/g)],
-        workload: [rule("regex", /\b(30|45|60|90)\b/g)],
-      });
+    if (auth.user.user_type == "administrador") {
+      try {
+        const validation = await validateAll(request.all(), {
+          code: "string|required|unique:disciplinas,code",
+          name: "string|required",
+          workload: "required|integer",
+          departamento_id: "required|integer",
+        });
+        const rules = await validateAll(request.only(["code", "workload"]), {
+          code: [rule("regex", /\b[A-Z]{3}[0-9]{3}\b/g)],
+          workload: [rule("regex", /\b(30|45|60|90)\b/g)],
+        });
 
-      if (validation.fails()) {
-        return response.status(401).send({ message: validation.messages() });
-      }
-      if (rules.fails()) {
-        return response.status(401).send({ message: rules.messages() });
-      }
+        if (validation.fails()) {
+          return response.status(401).send({ message: validation.messages() });
+        }
+        if (rules.fails()) {
+          return response.status(401).send({ message: rules.messages() });
+        }
 
-      const dataToCreate = request.all();
-      const disciplina = await Disciplina.create(dataToCreate, trx);
-      await trx.commit();
-      // return response.status(201).send({message: "Disciplina criada com sucesso!"})
-      return response.send(disciplina);
-    } catch (error) {
-      await trx.rollback();
-      return response.status(400).send({ error: `Erro: ${error.message}` });
-    }
+        const dataToCreate = request.all();
+        const disciplina = await Disciplina.create(dataToCreate, trx);
+        var log = {
+          log: `Usuário ${auth.user.username} de ID ${auth.user.id} adicionou a Disciplina ${disciplina.code} de ID ${disciplina.id}. Data de Criação: ${disciplina.created_at}`,
+        };
+        await Log.create(log, trx);
+        await trx.commit();
+        return response.status(200).send(disciplina);
+      } catch (error) {
+        await trx.rollback();
+        return response.status(400).send({ error: `Erro: ${error.message}` });
+      }
+    } else
+      response.status(401).send({
+        message:
+          "O tipo de usuário não tem permissão para executar esta funcionalidade",
+      });
   }
 
   /**
@@ -91,44 +101,54 @@ class DisciplinaController {
    * PUT or PATCH disciplinas/:id
    * ANCHOR UPDATE
    */
-  async update({ params, request, response }) {
+  async update({ params, request, response, auth }) {
     const trx = await Database.beginTransaction();
-    try {
-      const validation = await validateAll(request.all(), {
-        code: "string|unique:disciplinas,code",
-        name: "string",
-        workload: "integer",
+    if (auth.user.user_type == "administrador") {
+      try {
+        const validation = await validateAll(request.all(), {
+          code: "string|unique:disciplinas,code",
+          name: "string",
+          workload: "integer",
+        });
+        const rules = await validateAll(request.only(["code", "workload"]), {
+          code: [rule("regex", /\b[A-Z]{3}[0-9]{3}\b/g)],
+          workload: [rule("regex", /\b(30|60)\b/g)],
+        });
+
+        if (validation.fails()) {
+          return response.status(401).send({ message: validation.messages() });
+        }
+        if (rules.fails()) {
+          return response.status(401).send({ message: rules.messages() });
+        }
+
+        const dataToUpdate = request.all();
+        const disciplina = await Disciplina.findBy("id", params.id);
+
+        if (!disciplina) {
+          return response
+            .status(404)
+            .send({ message: "Nenhum registro localizado" });
+        }
+        disciplina.merge({ ...dataToUpdate });
+
+        await disciplina.save(trx);
+        var log = {
+          log: `Usuário ${auth.user.username} de ID ${auth.user.id} editou a Disciplina ${disciplina.code} de ID ${disciplina.id}. Data de Edição: ${disciplina.created_at}`,
+        };
+        await Log.create(log, trx);
+        await trx.commit();
+        return response.status(200).send(disciplina);
+        // response.status(201).send({message: 'Informações alteradas com sucesso!'})
+      } catch (error) {
+        await trx.rollback();
+        return response.status(400).send(`Erro: ${error.message}`);
+      }
+    } else
+      response.status(401).send({
+        message:
+          "O tipo de usuário não tem permissão para executar esta funcionalidade",
       });
-      const rules = await validateAll(request.only(["code", "workload"]), {
-        code: [rule("regex", /\b[A-Z]{3}[0-9]{3}\b/g)],
-        workload: [rule("regex", /\b(30|60)\b/g)],
-      });
-
-      if (validation.fails()) {
-        return response.status(401).send({ message: validation.messages() });
-      }
-      if (rules.fails()) {
-        return response.status(401).send({ message: rules.messages() });
-      }
-
-      const dataToUpdate = request.all();
-      const disciplina = await Disciplina.findBy("id", params.id);
-
-      if (!disciplina) {
-        return response
-          .status(404)
-          .send({ message: "Nenhum registro localizado" });
-      }
-      disciplina.merge({ ...dataToUpdate });
-
-      await disciplina.save(trx);
-      await trx.commit();
-      return response.status(200).send(disciplina);
-      // response.status(201).send({message: 'Informações alteradas com sucesso!'})
-    } catch (error) {
-      await trx.rollback();
-      return response.status(400).send(`Erro: ${error.message}`);
-    }
   }
 
   /**
@@ -136,26 +156,36 @@ class DisciplinaController {
    * DELETE disciplinas/:id
    * ANCHOR DESTROY
    */
-  async destroy({ params, request, response }) {
+  async destroy({ params, request, response, auth }) {
     const trx = await Database.beginTransaction();
-    try {
-      const disciplina = await Disciplina.findBy("id", params.id);
-      if (!disciplina) {
-        return response
-          .status(404)
-          .send({ message: "Nenhum registro localizado" });
-      } else if (disciplina.active == false) {
-        return response
-          .status(406)
-          .send({ message: "O disciplina já foi removida" });
+    if (auth.user.user_type == "administrador") {
+      try {
+        const disciplina = await Disciplina.findBy("id", params.id);
+        if (!disciplina) {
+          return response
+            .status(404)
+            .send({ message: "Nenhum registro localizado" });
+        } else if (disciplina.active == false) {
+          return response
+            .status(406)
+            .send({ message: "O disciplina já foi removida" });
+        }
+        disciplina.active = false;
+        await disciplina.save(trx);
+        var log = {
+          log: `Usuário ${auth.user.username} de ID ${auth.user.id} desativou a Disciplina ${disciplina.code} de ID ${disciplina.id}. Data de Desativação: ${disciplina.created_at}`,
+        };
+        await Log.create(log, trx);
+        await trx.commit();
+        return response.status(200).send({ message: "Disciplina desativada" });
+      } catch (error) {
+        return response.status(400).send(`Erro: ${error.message}`);
       }
-      disciplina.active = false;
-      await disciplina.save(trx);
-      await trx.commit();
-      return response.status(200).send({ message: "Disciplina desativada" });
-    } catch (error) {
-      return response.status(400).send(`Erro: ${error.message}`);
-    }
+    } else
+      response.status(401).send({
+        message:
+          "O tipo de usuário não tem permissão para executar esta funcionalidade",
+      });
   }
 
   /**
