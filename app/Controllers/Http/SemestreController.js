b76950b89664e4af15909336b797814d12adedf1
romaiajr <romaiajr5@gmail.com>
2021-05-03 00:08:10 -0300

    Semestre added to Log System

--- app/Controllers/Http/SemestreController.js
+++ app/Controllers/Http/SemestreController.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const Log = use("App/Models/SystemLog");
 const Semestre = use("App/Models/Semestre");
 const Database = use("Database");
 const { validateAll, rule } = use("Validator");
@@ -15,13 +16,7 @@ class SemestreController {
       const semestres = await Database.select("*")
         .table("semestres")
         .where("active", true);
-
-      // if (semestres.length == 0) {
-      //   return response
-      //     .status(200)
-      //     .send(semestres);
-      // }
-      return response.send(semestres);
+      return response.status(200).send(semestres);
     } catch (error) {
       return response.status(500).send(`Erro: ${error.message}`);
     }
@@ -32,41 +27,50 @@ class SemestreController {
    * POST semestre
    * ANCHOR STORE
    */
-  async store({ request, response }) {
+  async store({ request, response, auth }) {
     const trx = await Database.beginTransaction();
-    try {
-      const validation = await validateAll(request.all(), {
-        code: "string|required|unique:semestres,code",
-        start_date: "required|string",
-        end_date: "required|string|different:start_date",
-      });
+    if (auth.user.user_type == "administrador") {
+      try {
+        const validation = await validateAll(request.all(), {
+          code: "string|required|unique:semestres,code",
+          start_date: "required|string",
+          end_date: "required|string|different:start_date",
+        });
 
-      const rules = await validateAll(request.only(["code"]), {
-        code: [rule("regex", /([2]{1}[0]{1}[0-9]{2}[\.]{1}[1-3]{1})/g)],
-      });
+        const rules = await validateAll(request.only(["code"]), {
+          code: [rule("regex", /([2]{1}[0]{1}[0-9]{2}[\.]{1}[1-3]{1})/g)],
+        });
 
-      if (validation.fails()) {
-        return response.status(401).send({ message: validation.messages() });
+        if (validation.fails()) {
+          return response.status(401).send({ message: validation.messages() });
+        }
+        if (rules.fails()) {
+          return response.status(401).send({ message: rules.messages() });
+        }
+
+        const dataToCreate = await request.all();
+        const semestre = await Semestre.create(dataToCreate, trx);
+        var log = {
+          log: `Usuário ${auth.user.username} de ID ${auth.user.id} adicionou o Semestre ${semestre.code} de ID ${semestre.id}. Data de Criação: ${semestre.created_at}`,
+        };
+        await Log.create(log, trx);
+        await trx.commit();
+        return response.status(200).send(semestre);
+        // return response.send(semestre)
+      } catch (error) {
+        await trx.rollback();
+        return response.status(500).send(`Erro: ${error.message}`);
       }
-      if (rules.fails()) {
-        return response.status(401).send({ message: rules.messages() });
-      }
-
-      const dataToCreate = await request.all();
-      const semestre = await Semestre.create(dataToCreate, trx);
-      await trx.commit();
-      return response
-        .status(201)
-        .send(semestre);
-      // return response.send(semestre)
-    } catch (error) {
-      await trx.rollback();
-      return response.status(500).send(`Erro: ${error.message}`);
+    } else {
+      response.status(401).send({
+        message:
+          "O tipo de usuário não tem permissão para executar esta funcionalidade",
+      });
     }
   }
 
   /**
-   * Display a single departamento.
+   * Display a single Semestre.
    * GET semestre/:id
    * ANCHOR SHOW
    */
@@ -89,47 +93,58 @@ class SemestreController {
     }
   }
 
-  async update({ params, request, response }) {
+  async update({ params, request, response, auth }) {
     const trx = await Database.beginTransaction();
-    try {
-      const validation = await validateAll(request.all(), {
-        code: "string|unique:semestres,code",
-        start_date: "date",
-        end_date: "date|different:start_date",
-      });
+    if (auth.user.user_type == "administrador") {
+      try {
+        const validation = await validateAll(request.all(), {
+          code: "string|unique:semestres,code",
+          start_date: "date",
+          end_date: "date|different:start_date",
+        });
 
-      const rules = await validateAll(request.only(["code"]), {
-        code: [rule("regex", /([2]{1}[0]{1}[0-9]{2}[\.]{1}[1-2]{1})/g)],
-      });
+        const rules = await validateAll(request.only(["code"]), {
+          code: [rule("regex", /([2]{1}[0]{1}[0-9]{2}[\.]{1}[1-2]{1})/g)],
+        });
 
-      if (validation.fails()) {
-        return response.status(401).send({ message: validation.messages() });
+        if (validation.fails()) {
+          return response.status(401).send({ message: validation.messages() });
+        }
+
+        if (rules.fails()) {
+          return response.status(401).send({ message: rules.messages() });
+        }
+
+        const dataToUpdate = request.all();
+        const semestre = await Semestre.findBy("id", params.id);
+
+        if (!semestre) {
+          return response
+            .status(404)
+            .send({ message: "Nenhum registro localizado" });
+        }
+        semestre.merge({ ...dataToUpdate });
+
+        await semestre.save(trx);
+        var log = {
+          log: `Usuário ${auth.user.username} de ID ${auth.user.id} editou o Semestre ${semestre.code} de ID ${semestre.id}. Data de Edição: ${semestre.updated_at}`,
+        };
+        await Log.create(log, trx);
+        await trx.commit();
+        // return response.status(200).send(semestre);
+        return response.status(200).send({
+          message: "Informações alteradas com sucesso!",
+          semestre: semestre,
+        });
+      } catch (error) {
+        await trx.rollback();
+        return response.status(400).send(`Erro: ${error.message}`);
       }
-
-      if (rules.fails()) {
-        return response.status(401).send({ message: rules.messages() });
-      }
-
-      const dataToUpdate = request.all();
-      const semestre = await Semestre.findBy("id", params.id);
-
-      if (!semestre) {
-        return response
-          .status(404)
-          .send({ message: "Nenhum registro localizado" });
-      }
-      semestre.merge({ ...dataToUpdate });
-
-      await semestre.save(trx);
-      await trx.commit();
-      // return response.status(200).send(semestre);
-      return response.status(200).send({
-        message: "Informações alteradas com sucesso!",
-        semestre: semestre,
+    } else {
+      response.status(401).send({
+        message:
+          "O tipo de usuário não tem permissão para executar esta funcionalidade",
       });
-    } catch (error) {
-      await trx.rollback();
-      return response.status(400).send(`Erro: ${error.message}`);
     }
   }
 
@@ -138,25 +153,36 @@ class SemestreController {
    * DELETE semestre/:id
    * ANCHOR DESTROY
    */
-  async destroy({ params, request, response }) {
+  async destroy({ params, request, response, auth }) {
     const trx = await Database.beginTransaction();
-    try {
-      const semestre = await Semestre.findBy("id", params.id);
-      if (!semestre) {
-        return response
-          .status(404)
-          .send({ message: "Nenhum registro localizado" });
-      } else if (semestre.active == false) {
-        return response
-          .status(406)
-          .send({ message: "O semestre já foi removido" });
+    if (auth.user.user_type == "administrador") {
+      try {
+        const semestre = await Semestre.findBy("id", params.id);
+        if (!semestre) {
+          return response
+            .status(404)
+            .send({ message: "Nenhum registro localizado" });
+        } else if (semestre.active == false) {
+          return response
+            .status(406)
+            .send({ message: "O semestre já foi removido" });
+        }
+        semestre.active = false;
+        await semestre.save(trx);
+        var log = {
+          log: `Usuário ${auth.user.username} de ID ${auth.user.id} desativou o Semestre ${semestre.code} de ID ${semestre.id}. Data de Desativação: ${semestre.updated_at}`,
+        };
+        await Log.create(log, trx);
+        await trx.commit();
+        return response.status(200).send({ message: "Semestre desativado" });
+      } catch (error) {
+        return response.status(400).send(`Erro: ${error.message}`);
       }
-      semestre.active = false;
-      await semestre.save(trx);
-      await trx.commit();
-      return response.status(200).send({ message: "Semestre desativado" });
-    } catch (error) {
-      return response.status(400).send(`Erro: ${error.message}`);
+    } else {
+      response.status(401).send({
+        message:
+          "O tipo de usuário não tem permissão para executar esta funcionalidade",
+      });
     }
   }
 
